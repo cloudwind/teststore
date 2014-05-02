d5288768ead63da38b465503d9b0dcef486ec7c1
cloudwind <tospol_2535@hotmail.com>
2014-05-02 16:00:02 +0700

    first Jing Jing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '439ea7e9283182faa5c7391a998a445be562030b96a33c40e5f3a5febd5b3611634f60fb60d13c50f6d0189e7765120fe8e918129496145649d111ca02de445c'
